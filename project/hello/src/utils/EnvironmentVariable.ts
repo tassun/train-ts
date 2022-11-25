678e1aa86329309ce635cc1626bc85bc3899a79c
tassun <tassun_oro@hotmail.com>
2022-11-25 10:57:52 +0700

    initialize

--- project/hello/src/utils/EnvironmentVariable.ts
+++ project/hello/src/utils/EnvironmentVariable.ts
@@ -1,9 +1,9 @@
 import os from "os";
-export const DB_URL = process.env.DB_URL || "mysql://root:root@localhost:3306/accessdb?charset=utf8&connectionLimit=10&insecureAuth=true";
+export const DB_URL = process.env.DB_URL || "mysql://user:password@localhost:3306/testdb?charset=utf8&connectionLimit=10&insecureAuth=true";
 export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "root";
-export const DB_DATABASE = process.env.DB_DATABASE || "accessdb";
+export const DB_USER = process.env.DB_USER || "user";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "password";
+export const DB_DATABASE = process.env.DB_DATABASE || "testdb";
 export const DB_PORT = parseInt(process.env.DB_PORT || "3306") || 3306;
 export const HTTP_PORT = parseInt(process.env.HTTP_PORT || "8080") || 8080;
 export const RESOURCES_PATH = process.env.RESOURCES_PATH || os.tmpdir();
